f512876ae14e2edba82e241c488f4f027e705cc1
droppxr <bielbi67@gmail.com>
2025-12-18 22:44:20 -0300

    Inicio

--- api/index.py
+++ api/index.py
@@ -1,11 +1,50 @@
-from flask import Flask
+from flask import Flask, jsonify, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    url = url_for('api', _external=True)
+    return f'Rodrigo, o endpoint da API é <a href="{url}">{url}</a>'
 
-@app.route('/about')
-def about():
-    return 'About'
+@app.route("/plural")
+def api():
+    url = url_for("api", _external=True)
+    return f'ENDPOINT: {url}/{'{singular}'}'
+
+@app.route("/plural/<string:singular>")
+def singular_para_plural(singular):
+
+    dict = {'singular': '',
+            'plural': ''}
+    # Terminadas em ÃO
+
+
+    # Terminadas em A, E, I, O, U
+    if singular[-1] in "aeiou":
+        plural = singular + 's'
+        dict = {'singular': singular,
+                'plural': plural}
+        
+    # Terminadas em X
+    elif singular[-1] == 'x':
+        dict['singular'] = singular
+        dict['plural'] = singular
+    
+    # Terminadas em R, Z
+
+    # Terminadas em S
+
+    # Terminadas em M
+
+    # Terminadas em L
+
+    # Terminadas em IL
+
+    # Se for terminar de qualquer outra forma
+    else:
+        return (f'ERRO: Não conseguimos converter a palavra "{singular}" para plural')
+    return jsonify (dict)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=1234)
